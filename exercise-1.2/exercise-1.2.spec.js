d730dce1e08d2161aeb4e37291bd70dcc640e939
Thomas George <jorge0136@gmail.com>
2019-08-23 14:18:55 -0700

    Adds testing for exercise 1.2

---
+++ exercise-1.2/exercise-1.2.spec.js
@@ -0,0 +1,7 @@
+import { exerciseOneDotTwo } from "./exercise-1.2";
+
+describe('1.2 Translating Math Problems into JS', () => {
+
+    test('returns the correct answer as the formatting is correct', () =>
+        expect(exerciseOneDotTwo()).toEqual(-0.24666666666666667));
+});
